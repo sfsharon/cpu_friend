c082d1c805623dd2448eb6f26fdd5a6cb4380e76
sfsharon <sfsharon@gmail.com>
2019-02-23 02:59:43 +0200

    Initial commit

---
+++ adi_py.py
@@ -0,0 +1,26 @@
+"""
+Calculate working point for CPU power usage Vs. temperature
+23 Feb 2019
+"""
+
+class myDB :
+    """
+    Wrapper class for database 
+    """
+    db = {1:20.3, 2:40.55, 3:89.02}
+
+    def getVal(self, inputVal) :
+        """
+        Calculate working point according to inputVal
+        Input  : Input val : Int
+        Output : working point value : Float
+        If input val is not in DB, an approximation of it value will be returned
+        """
+        if inputVal in self.db :
+            print (self.db[inputVal])
+            return self.db[inputVal]
+            
+
+if __name__ == "__main__" :
+    dbObj = myDB()
+    dbObj.getVal(2)
