e0ca08206aa1d1087a0659ee0cc205182a698ff4
Sharon Fraiman <sfsharon@gmail.com>
2019-02-23 12:27:01 +0200

    Continue logic

--- adi_py.py
+++ adi_py.py
@@ -7,6 +7,8 @@ class myDB :
     """
     Wrapper class for database 
     """
+    # Implementing a function table values :
+    # Key in self.db is the non-dependant variable x, and value is the function value f(x)
     db = {1:20.3, 2:40.55, 3:89.02}
 
     def getVal(self, inputVal) :
@@ -14,7 +16,7 @@ class myDB :
         Calculate working point according to inputVal
         Input  : Input val : Int
         Output : working point value : Float
-        If input val is not in DB, an approximation of it value will be returned
+        If input val is not in DB, an approximation (implemeted average) of it value will be returned
         """
         if inputVal in self.db :
             print (self.db[inputVal])
@@ -24,9 +26,21 @@ class myDB :
             dbSize = len(dbSortedItemes)
             if dbSize == 0 :
                 return None
-            else
+            elif dbSize == 1 :
+                return self.db[list(self.db)[0]]
+            else :
+                sortedKeys = list(self.db.keys()).sort()
+                minKey = self.db[sortedKeys[0]]
+                maxKey = self.db[sortedKeys[-1]]
+                if inputValue <  minKey:
+                    return self.db[minKey]
+                elif inputValue >  maxKey:
+                    return self.db[maxKey]
+                elif inputValue in self.db.keys() :
+                    return self.db[inputValue]
+
 
 
 if __name__ == "__main__" :
     dbObj = myDB()
-    dbObj.getVal(2)
+    print ("Output is ", dbObj.getVal(2))
